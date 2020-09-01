ed218216088a7c2a161da10dd734b5a10559b66d
shashwath-t <shashwath.home@gmail.com>
2020-09-01 15:11:41 -0400

    first commit

---
+++ script.js
@@ -0,0 +1,29 @@
+function showTime(){
+    var date = new Date();
+    var hour = date.getHours(); 
+    var minute = date.getMinutes(); 
+    var second = date.getSeconds(); 
+    var session = "AM";
+    
+    if(hour == 0){
+        hour = 12;
+    }
+    
+    if(hour > 12){
+        hour = hour - 12;
+        session = "PM";
+    }
+    
+    hour = (hour < 10) ? "0" + hour : hour;
+    minute = (minute < 10) ? "0" + minute : minute;
+    second = (second < 10) ? "0" + second : second;
+    
+    var time = hour + ":" + minute + ":" + second + " " + session;
+    document.getElementById("DigitalCLOCK").innerText = time;
+    document.getElementById("DigitalCLOCK").textContent = time;
+    
+    setTimeout(showTime, 1000);
+    
+}
+ 
+showTime();
